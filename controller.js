ff41f370849320759ea344448312fb26f5e09a40
David Gonzalez <weerogoonzaleez@gmail.com>
2015-10-14 11:33:50 -0500

    creado la peticion al servidor alumnos.php y tabla

--- controller.js
+++ controller.js
@@ -1,6 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope) {
+myApp.controller('mainController', function($scope, $http) {
 
-
+$http.get("alumnos.php")
+   .success(function (response) {$scope.alumnos = response.records;});
 });
+
+
+
