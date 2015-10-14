18c55af84ec031dbbed0581721b6a0853f0d5d9d
vbgarciag <vbgarciag@gmail.com>
2015-10-14 11:36:02 -0500

    se creo script de alumno.php para mostrar json

---
+++ alumnos.php
@@ -0,0 +1,40 @@
+ <?php 
+
+$cnx=mysqli_connect("localhost","root","","codeigniter");
+/*if($cnx){
+  echo "conectado";
+}
+else{
+  echo "Fallo";
+}*/
+    
+
+
+
+
+
+		$query=("SELECT * from  datos_alumno");
+
+
+
+
+$result=$cnx->query($query);
+$row_cnt = $result->num_rows;
+
+if($row_cnt>0){
+	
+while($row=$result->fetch_assoc()){
+//    printf("<input type='text' size='25' id='input_cliente' value='".$row['nombre']."'>");
+	$arr[]= $row;
+
+}
+
+echo json_encode($arr); 
+}
+else{
+	echo "error";
+}
+
+
+
+ ?>
