b8b5ce2568d0b56af3b6fcc9fb932e16e4e66c4b
David Gonzalez <weerogoonzaleez@gmail.com>
2015-10-14 11:52:14 -0500

    se modifico el controller y el index

--- controller.js
+++ controller.js
@@ -3,7 +3,10 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController', function($scope, $http) {
 
 $http.get("alumnos.php")
-   .success(function (response) {$scope.alumnos = response.records;});
+   .success(function (response) {
+      $scope.alumnos = response.records;
+      console.log($scope.alumnos);
+   });
 });
 
 
